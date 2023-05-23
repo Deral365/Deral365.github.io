3bf6ea27011dd2423fe5f5dc3bc0bda83c1ed842
PerezDeral365 <124410553+Deral365@users.noreply.github.com>
2023-05-22 17:58:19 -0700

    Practica 1 P3 22/05/23

---
+++ Parcial 3/P001_P3.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<h1>Hola mundo</h1>";
+
+echo "<hr>";
+
+$nombre="Perez";
+
+$edad=16;
+
+if($edad >= 18){
+    echo $nombre . " es mayor de edad";
+}else{ 
+    echo $nombre ." es menor de edad";
+}
+echo "<br><br>";
+for($i=0;$i<10;$i++){
+    echo $i. "<br>";
+}
+
+?>
+
+
